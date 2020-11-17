a2828f455d9bf61c707a0cde0cb037a2fb7d839d
slskfl <hln9066@gmail.com>
2020-11-17 14:39:08 +0900

    Basic

---
+++ javaproject/javapro/src/javapro/Ex6.java
@@ -0,0 +1,15 @@
+package javapro;
+
+import java.util.Scanner;
+
+public class Ex6 /*클래스이름*/ {
+
+	public static void main/*메소드 이름*/(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 10;
+		if ( num>10 ) {
+			System.out.println("10보다  큰 수군요.");
+		}
+		System.out.println("끝.");
+	}
+}
