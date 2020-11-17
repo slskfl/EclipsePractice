3c3671fb75895daf85dc9a235f7f7f86052ed6d2
slskfl <hln9066@gmail.com>
2020-11-17 18:46:44 +0900

    Basic

--- javaproject/javapro/src/javapro/Ex8.java
+++ javaproject/javapro/src/javapro/Ex8.java
@@ -7,7 +7,8 @@ public class Ex8 /*클래스이름*/ {
 	public static void main/*메소드 이름*/(String[] args) {
 		// TODO Auto-generated method stub
 		byte num1 = 10;
-		byte num2 = num1;
+		//byte num2 = ~num1; >> ~num1은 에러
+		int num2 = num1;
 		System.out.println(num2);
 	}
 }
